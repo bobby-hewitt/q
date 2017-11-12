4d99ea4882615e744791c701213d050e048ffe92
Scott Wooden <scott@floxx.com>
2017-11-12 11:45:52 +0000

    invesment container / modal

---
+++ site/src/containers/Investment/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PageHeader from '../../components/PageHeader';
+import Testimonial from '../../components/Testimonial';
+import SectionBox from '../../components/SectionBox';
+import Quote from '../../components/Quote';
+import InvestmentSquare from '../../components/InvestmentSquare';
+import ApplySection from '../../components/ApplySection';
+import Footer from '../../components/Footer';
+import './style.css';
+
+export default class Investment extends Component {
+	render() {
+		return (
+			<div className="Investment">
+				<div className="Investments-video container-fluid">
+					<div className="row">
+						<div className="col-sm-12">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
